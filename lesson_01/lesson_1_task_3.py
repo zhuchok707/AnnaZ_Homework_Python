382fd0577584795d2c21221cd814e065786f9f8f
zhuchok707 <zhuchok707@gmail.com>
2025-11-03 18:38:22 +0500

    Add files for lesson 1 homework

---
+++ lesson_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input("Anna: ")
+last_name = input("Zhukova: ")
+print(f"Вас зовут: {last_name} {first_name}")
---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,6 @@
+def print_number(num):
+    print(num)
+
+
+for _ in range(11):
+    print_number("88005553535")
