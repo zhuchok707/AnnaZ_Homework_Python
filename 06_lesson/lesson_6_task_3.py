1dcd28304fc478e52a58445ac60fa9b040473721
zhuchok707 <zhuchok707@gmail.com>
2025-11-27 11:49:52 +0500

    исправила 3 задание

--- 06_lesson/lesson_6_task_3.py
+++ 06_lesson/lesson_6_task_3.py
@@ -1,26 +1,32 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
-driver.get(
-    "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
-    )
 
-third_src = None
-timeout = 30
-start_time = time.time()
+url = (
+    "https://bonigarcia.dev/selenium-webdriver-java/"
+    "loading-images.html"
+)
+driver.get(url)
 
-while time.time() - start_time < timeout:
-    images = driver.find_elements(By.CSS_SELECTOR, "#image-container img")
-    loaded_images = [img for img in images if img.get_attribute("src")]
-    if len(loaded_images) >= 3:
-        third_src = loaded_images[2].get_attribute("src")
-        break
+wait = WebDriverWait(driver, 10)
 
-if third_src:
-    # Берём только имя файла
-    file_name = third_src.split("/")[-1]
-    print(file_name)  # вывод: award.png
+
+img3 = wait.until(
+    EC.presence_of_element_located((By.ID, "award"))
+)
+
+
+wait.until(
+    lambda d: img3.get_attribute("src") not in (None, "")
+)
+
+src = img3.get_attribute("src")
+
+
+filename = src.split("/")[-1]
+print(filename)
 
 driver.quit()
